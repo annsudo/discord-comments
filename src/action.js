88824fd492582d6d2655e77c2f2a6d3c4e70feb4
Anna <anna.nikolskaya@hotmail.com>
2021-04-05 11:27:01 +0200

    commit

---
+++ src/action.js
@@ -0,0 +1,30 @@
+const core = require ('@actions/core');
+const github= require('@actions/github');
+const fetch= require('node-fetch');
+
+async function run(){
+try{
+
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  
+  const client= github.getOctokit(GITHUB_TOKEN);
+  let { owner, repo } = github.context.repo;
+
+//   await client.issues.createComment({
+//     owner,
+//     repo,
+//     issue_number: number,
+//    // body
+//    body: `${changeMsg}\n\n<img src="${gifUrl}" alt = "almost there" />`
+// });
+
+
+
+
+} catch (e){
+    core.error(e);
+    core.setFailed(e.message);
+}
+};
+
+run();
